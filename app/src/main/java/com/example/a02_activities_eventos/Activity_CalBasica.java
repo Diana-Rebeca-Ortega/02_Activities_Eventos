eb74e9e79920f8da815726e060311238a6761498
Diana Rebeca Ortega <diana.os@tecjerez>
2025-05-11 20:08:14 -0600

    3º commit: Logica de la calculadora terminada, todos los botones son funcionales

---
+++ app/src/main/java/com/example/a02_activities_eventos/Activity_CalBasica.java
@@ -0,0 +1,187 @@
+package com.example.a02_activities_eventos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+
+public class Activity_CalBasica extends Activity {
+    private EditText cajaResultado;
+    private Button btn1, btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
+    private Button btnDividir,btnMultiplicar,btnMas,btnMenos, btnPorcentaje, btnRaiz , btnReiniciar, btnAjustes;
+    private Button btn_Igual, btnCuadrado, btnUnoSobreX, btnPunto, btnBorrar, btnMasMenos;
+    private Double primero, segundo;
+   private String operador;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cal_basica);
+        cajaResultado = findViewById(R.id.cajaResB);
+        //Enlazar numeros
+        btn1= findViewById(R.id.btn_1);
+        btn2= findViewById(R.id.btn_2);
+        btn3= findViewById(R.id.btn_3);
+        btn4= findViewById(R.id.btn_4);
+        btn5= findViewById(R.id.btn_5);
+        btn6= findViewById(R.id.btn_6);
+        btn7= findViewById(R.id.btn_7);
+        btn8= findViewById(R.id.btn_8);
+        btn9= findViewById(R.id.btn_9);
+        btn0= findViewById(R.id.btn_0);
+        //Enlazar Operadores
+      btnMultiplicar= findViewById(R.id.btn_multiplicar);
+      btnMas = findViewById(R.id.btn_Suma);
+      btnMenos = findViewById(R.id.btn_Resta);
+      btnDividir = findViewById(R.id.btn_Division);
+
+      btnPorcentaje= findViewById(R.id.btn_Porciento);
+      btnRaiz = findViewById(R.id.btn_Raiz);
+      btnCuadrado = findViewById(R.id.btn_Cuadrado);
+      btnUnoSobreX = findViewById(R.id.btn_Uno_Entre_X);
+
+      btnPunto = findViewById(R.id.btn_Punto);
+      btnBorrar = findViewById(R.id.btn_Borrar);
+      btnMasMenos = findViewById(R.id.btn_MasMenos);
+    }
+//*********************Numeros************************
+    public void indicarNumero(View v ){
+        if(v.getId() == R.id.btn_1){
+            cajaResultado.setText(cajaResultado.getText()+"1");
+            borrarCero();
+        }
+        if(v.getId() == R.id.btn_2){
+            cajaResultado.setText(cajaResultado.getText()+"2");
+            borrarCero();
+        }if(v.getId() == R.id.btn_3){
+            cajaResultado.setText(cajaResultado.getText()+"3");
+            borrarCero();
+        }if(v.getId() == R.id.btn_4){
+            cajaResultado.setText(cajaResultado.getText()+"4");
+            borrarCero();
+        }if(v.getId() == R.id.btn_5){
+            cajaResultado.setText(cajaResultado.getText()+"5");
+            borrarCero();
+        }if(v.getId() == R.id.btn_6){
+            cajaResultado.setText(cajaResultado.getText()+"6");
+            borrarCero();
+        }if(v.getId() == R.id.btn_7){
+            cajaResultado.setText(cajaResultado.getText()+"7");
+            borrarCero();
+        }if(v.getId() == R.id.btn_8){
+            cajaResultado.setText(cajaResultado.getText()+"8");
+            borrarCero();
+        }if(v.getId() == R.id.btn_9){
+            cajaResultado.setText(cajaResultado.getText()+"9");
+            borrarCero();
+        }if(v.getId() == R.id.btn_0){
+            cajaResultado.setText(cajaResultado.getText()+"0");
+            borrarCero();
+        }if(v.getId() == R.id.btn_Punto){
+            cajaResultado.setText(cajaResultado.getText()+".");
+        }
+    }//numeros
+    //***********************Operadores****************************
+  public void indicarOperador(View v ) {
+        //operadores basicos
+   if(v.getId() == R.id.btn_x){
+       primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+       cajaResultado.setText("0");
+       operador = "*";
+   }if(v.getId() == R.id.btn_Suma){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "+";
+   }if(v.getId() == R.id.btn_Resta){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "-";
+   }if(v.getId() == R.id.btn_Division){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "/";
+   }
+   //Operadores nivel 2
+      if(v.getId() == R.id.btn_Porciento){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "P";
+      }if(v.getId() == R.id.btn_Raiz){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "R";
+      }if(v.getId() == R.id.btn_Cuadrado){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "C";
+      }if(v.getId() == R.id.btn_Uno_Entre_X){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText("0");
+          operador = "X";
+      }if(v.getId() == R.id.btn_MasMenos){
+          primero = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+          cajaResultado.setText(String.valueOf(  primero*(-1)));
+          operador = "q";
+      }
+
+    }//indicar operador
+
+   //*****************Otras funciones*****************************
+
+    public void hacerCE (View v){
+        cajaResultado.setText("0");
+    }
+    public void hacerC (View v){
+        cajaResultado.setText("0");
+        primero = 0.0;
+        segundo= 0.0;
+    }
+    public void borrar (View v){
+        String cadena = String.valueOf(cajaResultado.getText());
+        if(v.getId()==R.id.btn_Borrar) {
+            if(cadena.length()==1 || cadena.charAt(0)=='0' ) {
+                cajaResultado.setText("0");
+            }else{
+                cajaResultado.setText(cadena.substring(0,cadena.length()-1));
+            }
+        }
+    }
+    public void  borrarCero (){
+        String cadena =String.valueOf(cajaResultado.getText());
+        if(cadena.length()==2  ) {
+            if(cadena.charAt(0)=='0') {
+                cadena= String.valueOf(cadena.charAt(1));
+                cajaResultado.setText(cadena);	}
+        }
+    }//borrar 0
+//***********************Indicar resultado***********************
+
+    public void ObtenerResultado (View v){
+        if (v.getId() == R.id.btn_Igual) {
+            segundo = Double.parseDouble(String.valueOf(cajaResultado.getText()));
+            if (operador=="*"){
+                cajaResultado.setText(String.valueOf(	primero*segundo));
+            }if (operador=="+"){
+                cajaResultado.setText(String.valueOf(	primero+segundo));
+            }if (operador=="-"){
+                cajaResultado.setText(String.valueOf(	primero-segundo));
+            }if (operador=="/"){
+                cajaResultado.setText(String.valueOf(	primero/segundo));
+            }
+
+            if (operador=="P"){
+                cajaResultado.setText(String.valueOf(	primero/100));
+            }if (operador=="R"){
+                cajaResultado.setText(String.valueOf(	Math.sqrt(primero)));
+            }if (operador=="C"){
+                cajaResultado.setText(String.valueOf(	primero*primero));
+            }if (operador=="X"){
+                cajaResultado.setText(String.valueOf(	1/primero));
+            }
+        }
+
+    }
+
+}
